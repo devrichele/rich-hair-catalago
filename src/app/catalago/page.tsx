c3b37a1bf40e4f7ecb4f4b2c314cac7e51eb97c3
Richele <brendaricheledev@gmail.com>
2024-09-01 10:21:00 -0300

    criando lista

--- src/app/catalago/page.tsx
+++ src/app/catalago/page.tsx
@@ -1,7 +1,33 @@
-import React from 'react'
+import React from 'react';
 
-export default function page() {
+export default function Page() {
+  const servicos = [
+    { nome: "Box Braids", preco: 'R$ 300,00' },
+    { nome: "Box Braids Chanel", preco: 'R$ 320,00' },
+    { nome: "Gypsy Braids", preco: 'R$ 380,00' },
+    { nome: "Nagô Femnina", preco: 'R$ 180,00' },
+    { nome: "Ghana Braids", preco: 'R$ 280,00' },
+    { nome: "Boho Braids (com orgânico)", preco: 'R$ 400,00' },
+
+
+
+
+  ];
+
   return (
-    <div className='text-amber-950 text-3xl'>RICH HAIR</div>
-  )
+    <div className='text-amber-950 text-3xl'>
+      RICH HAIR
+      <p>Tabela de Preços</p>
+      <div className='listTable'>
+        <ul>
+          {/* Você pode mapear os serviços para criar a lista dinamicamente */}
+          {servicos.map((servico, index) => (
+            <li key={index}>
+              {servico.nome}: {servico.preco}
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
 }
